5ea9fe0f11df759ee439bd44fbc341fffad5b46b
Yakoub <yakoub@MacBook-Pro-de-Yakoub.local>
2020-10-12 15:52:11 +0200

    add UItest/ test fonctionelles

--- Reciplease/RecipleaseUITests/RecipleaseUITests.swift
+++ Reciplease/RecipleaseUITests/RecipleaseUITests.swift
@@ -23,15 +23,134 @@ class RecipleaseUITests: XCTestCase {
 
     }
     
-        func testSearchButton() {
+        func testFavoriteButton() {
             app = XCUIApplication()
             app.launch()
-            app.buttons["Search"].tap()
+            app.buttons["Favorites"].tap()
+            XCTAssert(app.buttons["Favorites"].exists)
+            
         }
     
-    func testSearchAdd() {
+        func testFavoriteButtonthenSearchButton() {
+            app = XCUIApplication()
+            app.launch()
+            app.buttons["Favorites"].tap()
+            XCTAssert(app.buttons["Favorites"].exists)
+            sleep(1)
+            app.buttons["Search"].tap()
+            sleep(1)
+            XCTAssert(app.buttons["Search"].exists)
+            sleep(1)
+
+    }
+    
+    
+    func testSearchRecip() {
         app = XCUIApplication()
         app.launch()
+        let ingredients = "Chicken"
+        let textField = app.textFields["ingredientText"]
+        textField.tap()
+        for i in ingredients {
+            textField.typeText(i.description)
+            sleep(1/2)
+
+        }
+        XCTAssertEqual(textField.value as! String, "Chicken", "Equal")
         app.buttons["addButton"].tap()
+        app.tap()
+        app.buttons["searchButton"].tap()
+        sleep(3)
+        app.tap()
+        sleep(2)
+        app.buttons["Get directions"].tap()
+        sleep(2)
+        app.buttons["Done"].tap()
+        sleep(1)
     }
+    
+    func testButtonExist() {
+        app = XCUIApplication()
+        app.launch()
+        XCTAssert(app.textFields["ingredientText"].exists)
+        XCTAssert(app.buttons["addButton"].exists)
+        XCTAssert(app.buttons["clearButton"].exists)
+        XCTAssert(app.buttons["searchButton"].exists)
+    }
+    
+    func testClear() {
+        app = XCUIApplication()
+        app.launch()
+        let textField = app.textFields["ingredientText"]
+        textField.tap()
+        textField.typeText("Chicken")
+        sleep(1)
+        app.buttons["addButton"].tap()
+        sleep(1)
+        app.buttons["clearButton"].tap()
+        XCTAssertEqual(textField.value as! String, "Lemon, Cheese, Sausages...", "Equal")
+
+    }
+        
+    func testSearchRecipAndBack() {
+            app = XCUIApplication()
+            app.launch()
+            let ingredients = "Chicken"
+            let textField = app.textFields["ingredientText"]
+            textField.tap()
+            for i in ingredients {
+                textField.typeText(i.description)
+                sleep(1/2)
+
+            }
+            XCTAssertEqual(textField.value as! String, "Chicken", "Equal")
+            app.buttons["addButton"].tap()
+            app.tap()
+            app.buttons["searchButton"].tap()
+            sleep(3)
+            app.tap()
+            sleep(2)
+            app.buttons["Back"].tap()
+            sleep(1)
+        }
+    
+    func testSearchRecipFavorit() {
+        app = XCUIApplication()
+        app.launch()
+        let ingredients = "Chicken"
+        let textField = app.textFields["ingredientText"]
+        textField.tap()
+        for i in ingredients {
+            textField.typeText(i.description)
+        }
+        app.buttons["addButton"].tap()
+        app.tap()
+        app.buttons["searchButton"].tap()
+        sleep(3)
+        app.tap()
+        app.buttons["favorit"].tap()
+        sleep(2)
+        app.buttons["Favorites"].tap()
+        sleep(2)
+        app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+        app.buttons["Get directions"].tap()
+        sleep(2)
+        app.buttons["Done"].tap()
+        sleep(2)
+    }
+    
+    func testSearchRecipRemoveFavorit() {
+        app = XCUIApplication()
+        app.launch()
+       
+        app.buttons["Favorites"].tap()
+        sleep(2)
+        app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+        sleep(2)
+        app.buttons["favorit"].tap()
+        sleep(2)
+        app.buttons["Back"].tap()
+        sleep(2)
+    }
+    
 }
